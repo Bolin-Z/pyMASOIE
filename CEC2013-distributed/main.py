58aabf684537eaca7eb98d614ae415ad4d90d700
Bolin-Z <csbolinz@mail.scut.edu.cn>
2023-11-10 20:32:24 +0800

    main

--- CEC2013-distributed/main.py
+++ CEC2013-distributed/main.py
@@ -1,5 +1,7 @@
+import ray
 from typing import Callable
 from CEC2013 import CEC2013
+from MASOIE.agent import Agent
 
 class LocalEvaluator:
     def __init__(self, id:int, localEvaluate:Callable[[list[float], int], float]) -> None:
@@ -10,3 +12,23 @@ class LocalEvaluator:
 
 if __name__ == "__main__":
     problem = CEC2013("50D20n1d-6")
+
+    numberOfAgents =  problem.getGroupNum()
+    dimension = problem.getDimension()
+    lowerBound = problem.getMinX()
+    upperBound = problem.getMaxX()
+    
+    localEvaluators = [LocalEvaluator(i, problem.local_eva) for i in range(numberOfAgents)]
+    netWorkGraph = problem.getNetworkGraph()
+    neighborsIDList = [[] for _ in range(numberOfAgents)]
+    neighborsWeightList = [[] for _ in range(numberOfAgents)]
+    for i in range(numberOfAgents):
+        for j in range(numberOfAgents):
+            if i!=j and netWorkGraph[i][j] != 0:
+                neighborsIDList[i].append(j)
+                neighborsWeightList[i].append(netWorkGraph[i][j])
+    
+    agents = [Agent.remote(
+        dimension,
+        lowerBound,
+    ) for i in range(numberOfAgents)]
